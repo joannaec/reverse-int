a457bdf497e6b39e99a8adb62f9507567460f585
joannaec <yhanah.mu@gmail.com>
2025-07-27 20:54:56 +0100

    fix: fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 module.exports = function reverse(n) {
-  let numberToString = Math.abs(n).toString();
-
-  let newString = "";
-  for (let i = numberToString.length - 1; i >= 0; i--) {
-    newString = newString + numberToString[i];
+  const numberToString = Math.abs(n).toString();
+  let newString = '';
+  for (let i = numberToString.length - 1; i >= 0; i -= 1) {
+    newString += numberToString[i];
   }
   return Number(newString);
 };
-
-
-
-
-
-
