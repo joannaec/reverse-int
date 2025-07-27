fa50deff32a9a7d6eac61b7e0346ae968a11b00f
joannaec <yhanah.mu@gmail.com>
2025-07-27 20:11:46 +0100

    fix: update solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 module.exports = function reverse(n) {
-  let numberToString = n.toString();
+  n = +n;
+  let numberToString = Math.abs(n).toString();
+  console.log(numberToString);
   let newString = "";
-  for (let i = numberToString.length - 1; i <= 0; i--) {
+  for (let i = numberToString.length - 1; i >= 0; i--) {
     newString = newString + numberToString[i];
   }
   return Number(newString);
 };
+
+
+
+
+
+
